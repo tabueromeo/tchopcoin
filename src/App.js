98fdedb8139cbbb775a5db5b1f5ca4b25d7b369d
tabueromeo <tabueromeo@gmail.com>
2022-07-13 11:01:32 +0100

    premier commit mise en place des bases

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/tchopcoin.png" className="logo" alt="tchopcoin Sarl" />
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
